eecae727d03ab996fefa2204dae17e123d572e82
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-08-04 01:42:25 +0600

    install the multer and cloudnary

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,23 @@
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
+  const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+    const lastPath = issue.path[issue.path.length - 1];
+
+    return {
+      path: typeof lastPath === 'symbol' ? lastPath.toString() : lastPath,
+      message: issue.message,
+    };
+  });
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
+
+export default handleZodError;
