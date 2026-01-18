fd72a92b1daa27429f3f9897dafd45ffe2c650e6
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2026-01-18 20:56:46 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,73 @@
-import express, { Application, Request, Response } from 'express';
+// import express, { Application, Request, Response } from 'express';
 
 
-import cors from 'cors';
-import router from './app/routes';
-import notFound from './app/middlewares/notFound';
+// import cors from 'cors';
+// import router from './app/routes';
+// import notFound from './app/middlewares/notFound';
+// import globalErrorHandler from './app/middlewares/globalErrorhandler';
+// const app: Application = express();
+// //Middleware to parse incoming JSON request
+// app.use(express.json());
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:3000'
+//     ],
+//   }),
+// );
+
+// //Application Routes
+// app.use('/api/v1', router);
+
+// //Base route
+// app.get('/', (req: Request, res: Response) => {
+//   res.json({
+//     status: true,
+//     message: 'Portfolio Server Live ⚡',
+//   });
+// });
+
+// // Use the Not Found Middleware
+// app.use(notFound);
+// // Use the Global Error Handler
+// app.use(globalErrorHandler);
+
+// export default app;
+
+
+import express, { application, Request, Response } from 'express';
+import sendResponse from './app/utils/sendResponse';
+import cors from "cors";
+import status from 'http-status';
+import cookieParser from 'cookie-parser';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
-const app: Application = express();
-//Middleware to parse incoming JSON request
+import notFound from './app/middlewares/notFound';
+import serverRoutes from './app/router/routes';
+const app = express()
+
+// meddleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-      'https://portfolio-client-mocha.vercel.app',
-    ],
-  }),
-);
+app.use(express.raw());
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors())
+app.use("/", serverRoutes)
 
-//Application Routes
-app.use('/api/v1', router);
 
-//Base route
+
+// test route
 app.get('/', (req: Request, res: Response) => {
-  res.json({
-    status: true,
-    message: 'Portfolio Server Live ⚡',
-  });
-});
+    sendResponse(res, {
+        success: true,
+        message: "Server is successfully running 🏃‍♀️‍➡️🏃‍♀️‍➡️🏃‍♀️‍➡️",
+        statusCode: status.OK,
+        data: null
+    })
+})
 
-// Use the Not Found Middleware
-app.use(notFound);
-// Use the Global Error Handler
-app.use(globalErrorHandler);
+// handel error globally
+app.use(globalErrorHandler)
+app.use(notFound)
 
-export default app;
+export default app
---
+++ src/app/router/routes.ts
@@ -0,0 +1,42 @@
+import authRoute from '../modules/auth/auth.route'
+import blogRouter from '../modules/blog/blog.route'
+import messageRoute from '../modules/message/message.route'
+
+import projectRoute from '../modules/project/project.route'
+import { Router } from 'express'
+import skill_router from '../modules/skill/skill.route'
+
+
+const serverRoutes = Router()
+
+type TServerRoutes = {
+  path: string
+  route: any
+}
+const allRoutes: TServerRoutes[] = [
+  {
+    path: '/auth',
+    route: authRoute
+  },
+  {
+    path: '/blogs',
+    route: blogRouter
+  },
+  {
+    path: '/project',
+    route: projectRoute
+  },
+  {
+    path: "/message",
+    route: messageRoute
+  },
+  {
+    path: "/skill",
+    route: skill_router
+  }
+]
+
+// dynamically create route
+allRoutes.forEach(route => serverRoutes.use(route.path, route.route))
+
+export default serverRoutes
